94b63c2912a0993131266122243df2ad1938e139
Mrin <mrin9@yahoo.com>
2018-12-18 09:23:31 -0800

    fixed default doc

--- src/components/AppShell.vue
+++ src/components/AppShell.vue
@@ -223,8 +223,8 @@ export default {
     else{
       
       //this.specUrl="http://developer.twinehealth.com/swagger.json";
-      //this.specUrl= "https://petstore.swagger.io/v2/swagger.json";
-      this.specUrl= "http://10.21.83.83:8080/api/swagger.json";
+      this.specUrl= "https://petstore.swagger.io/v2/swagger.json";
+      //this.specUrl= "http://10.21.83.83:8080/api/swagger.json";
       //this.specUrl= "https://raw.githubusercontent.com/APIs-guru/unofficial_openapi_specs/master/github.com/v3/swagger.yaml", //large spec
       //this.specUrl= "https://fakerestapi.azurewebsites.net/swagger/docs/v1";
       //this.specUrl= "https://api.apis.guru/v2/specs/twilio.com/2010-04-01/swagger.json";  //xml responses
